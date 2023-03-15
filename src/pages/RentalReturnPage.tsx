62e5a8691cba97f8439a00b30f3466c1c35a3089
jjunghoo <wjdgn2468@naver.com>
2023-03-15 14:16:49 +0900

    style: 렌탈 반납 페이지 ui

--- src/pages/RentalReturnPage.tsx
+++ src/pages/RentalReturnPage.tsx
@@ -1,9 +1,19 @@
 import React from "react";
+import RentalProductItem from "../components/RentalProductItem";
 
 export default function RentalReturnPage() {
   return (
     <>
-      <h1>RentalReturn</h1>
+      <h2 className="text-2xl mt-4 mb-8">반납하기</h2>
+      <RentalProductItem />
+      <div className="text-center my-16">
+        <input
+          type="text"
+          placeholder="상품 운송장 번호를 입렵해주세요."
+          className="input input-bordered w-full max-w-xs block mx-auto"
+        />
+        <button className="btn btn-primary w-40 mt-11">반납하기</button>
+      </div>
     </>
   );
 }
