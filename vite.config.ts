554ad44b4249a41526ae5de30365e997b18ff981
Jeong-Yeon-Lee <0906ljy@gmail.com>
2023-03-28 00:13:50 +0900

    :wrench: build:안되어서 다시 fallback

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import ts from "vite-plugin-ts-checker";
+//import ts from "vite-plugin-ts-checker";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), ts()],
+  plugins: [react()],
   define: {
     global: {},
   },
