a7f23267c8e123249851b5b907d3c27363ba4006
Jeong-Yeon-Lee <0906ljy@gmail.com>
2023-03-31 16:12:01 +0900

    :twisted_rightwards_arrows: test: merge 및 token 관련 todo 작성

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,12 +1,17 @@
 import axios, { AxiosResponse, AxiosRequestConfig, AxiosInstance } from "axios";
 
-const API_BASE_URL = "";
-const API_TOKEN = "your_api_token"; // 서버에서 받아온 안전한 accountToken 사용
+//TODO : user token, user info 다루는 atom 가져와서, header에 토큰 실어주면 되겠네요!
+//import { token } from "../atoms/token";
+//import { useRecoilValue } from "recoil";
 
+//TODO: 서버 주소 입력
+const API_BASE_URL = "";
+//const API_TOKEN = useRecoilValue(token); // 서버에서 받아온 안전한 accountToken 사용
+const API_TOKEN = "token"; // 임시
 export const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
   headers: {
-    //Authorization: `Bearer ${API_TOKEN}`,
+    //Authorization: `${API_TOKEN}`,
     "Content-Type": "application/json",
   },
   withCredentials: false,
@@ -15,17 +20,8 @@ export const axiosInstance = axios.create({
 export const axiosFormInstance = axios.create({
   baseURL: API_BASE_URL,
   headers: {
-    //Authorization: `Bearer ${API_TOKEN}`,
+    //Authorization: `${API_TOKEN}`,
     "Content-Type": "multipart/form-data",
   },
   withCredentials: false,
 });
-
-// export async function fetchMore<T>(
-//   url: string,
-//   options?: AxiosRequestConfig
-// ): Promise<T> {
-//   const response: AxiosResponse<T> = await axios.get(url, options);
-//   //console.log(response.data.data);
-//   return response.data;
-// }
