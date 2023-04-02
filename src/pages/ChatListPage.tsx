8adc9fa5339a4e268458eed86a372b1008b03544
jjunghoo <wjdgn2468@naver.com>
2023-04-02 16:36:18 +0900

    :white_check_mark: test: 로그인한 유저가 참여하고 있는 채팅방 리스트 부분 테스트중....

--- src/pages/ChatListPage.tsx
+++ src/pages/ChatListPage.tsx
@@ -5,29 +5,37 @@ import { useRecoilState, useRecoilValue } from "recoil";
 import { userInfo } from "../atoms/userInfo";
 import { ChatListItem } from "../components/ChatListItem";
 
-const chatList = [
-  {
-    id: 1,
-    productId: 1,
-    lastChatMsg: "안녕하세요.",
-  },
-  {
-    id: 2,
-    productId: 2,
-    lastChatMsg: "반갑습니다.",
-  },
-  {
-    id: 3,
-    productId: 3,
-    lastChatMsg: "상품 렌탈 문의 드립니다.",
-  },
-];
+// const chatList = [
+//   {
+//     id: 1,
+//     productId: 1,
+//     lastChatMsg: "안녕하세요.",
+//   },
+//   {
+//     id: 2,
+//     productId: 2,
+//     lastChatMsg: "반갑습니다.",
+//   },
+//   {
+//     id: 3,
+//     productId: 3,
+//     lastChatMsg: "상품 렌탈 문의 드립니다.",
+//   },
+// ];
+
 export default function ChatListPage() {
-  // const { userEmail } = useRecoilValue(userInfo);
-  // // userEmail 사용해서 채팅 리스트 받아와야함
-  // const { data, isLoading, isError, error } = useQuery(["chatList", userEmail], () => {
-  //   axios.get(`/chat/list?userId=${userEmail}`)
-  // });
+  const { userEmail } = useRecoilValue(userInfo);
+  // userEmail 사용해서 채팅 리스트 받아와야함
+  const { data, isLoading, isError, error } = useQuery(
+    ["chatList", userEmail],
+    async () => {
+      await axios.get(
+        `http://43.200.141.247:8080/chat/list?userId=${userEmail}`
+      );
+    }
+  );
+
+  console.log(data);
 
   return (
     <div className="flex flex-col items-center px-5 md:px-28 lg:px-40">
@@ -37,14 +45,14 @@ export default function ChatListPage() {
       {/* 할거 */}
       {/* data를 활용 */}
       <ul className="divide-y">
-        {chatList.map((list) => (
+        {/* {data.map((list: any) => (
           <ChatListItem
             key={list.id}
             roomId={list.id}
             productId={list.productId}
             lastChatMsg={list.lastChatMsg}
           />
-        ))}
+        ))} */}
       </ul>
     </div>
   );
