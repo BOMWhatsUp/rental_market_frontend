6e662b6aaaae0a110379462e84c98d654789011c
jjunghoo <wjdgn2468@naver.com>
2023-04-03 16:39:31 +0900

    :white_check_mark: test: 상품 리스트, 상품 상세 페이지 (채팅 테스트를 위해  목업 데이터 사용)

---
+++ src/pages/MainPage copy.tsx
@@ -0,0 +1,459 @@
+import React, { useState, useEffect } from "react";
+import RentalProductItem from "../components/RentalProductItem";
+import { Bars3Icon, MapIcon, FlagIcon } from "@heroicons/react/24/outline";
+import ProductList from "../components/ProductList";
+import { RentalProduct } from "../types/product";
+import { InfiniteData, useInfiniteQuery, useQueryClient } from "react-query";
+import { getProducts } from "../api/rentalProduct/rentalProductAPI";
+import Badge from "../components/Badge";
+import { productStatus } from "../utils/converter";
+import { userInfo } from "../atoms/userInfo";
+import { useRecoilState } from "recoil";
+
+// 테스트
+const data = [
+  {
+    categoryName: "HOME",
+    content: "새거새거",
+    createdAt: "2023-03-31T14:40:27.79989",
+    id: 1,
+    mainImageUrl: "6f67cf96-56b2-4f58-8",
+    maxRentalPeriod: "ONEMONTH",
+    modifiedAt: "2023-03-31T14:40:27.799921",
+    nickname: "개구리페페",
+    // returnDate: null,
+    sellerId: "pepe@gmail.com",
+    status: "AVAILABLE",
+    title: "냉장고",
+    unitPrice: 12300,
+    wishRegion: "서울 도봉구 노해로 133",
+  },
+];
+
+// 테스트
+
+type ProductsFilter = {
+  category: string;
+  region: string;
+  keyword: string;
+  status: string;
+};
+export default function MainPage() {
+  const [filter, setFilter] = useState<ProductsFilter>({
+    category: "",
+    region: "",
+    keyword: "",
+    status: "",
+  });
+  //TODO: 로그인 없이 임시 테스트를 위한 유저 info - 유저정보로 바꿔야
+  const [user, setUser] = useRecoilState(userInfo);
+  useEffect(() => {
+    // setUser({
+    //   userEmail: "pepe@gmail.com",
+    //   userNickName: "개구리페페",
+    //   userRegion: "서울시 도봉구",
+    //   userProfileImage:
+    //     "https://blog.kakaocdn.net/dn/wR5bN/btqSxCsIZD8/0g1pTeaqRwXKvBcxPtqQE0/img.jpg",
+    // });
+  }, []);
+  //request query param- 바로 적용 안되는 문제 해결
+  // let categoryName = "";
+  let wishRegion = "";
+  // let keyword = "";
+  // let status = "";
+
+  const queryClient = useQueryClient();
+  //const queryClient = useQueryClient();
+  // const [filter, setFilter] = useState({
+  //   category: "",
+  //   region: "",
+  //   keyword: "",
+  //   status: "",
+  // });
+  // const handleFilterChange = (target: HTMLAnchorElement) => {
+  //   setFilter((prevFilter) => ({
+  //     ...prevFilter,
+  //     [`${target.dataset.title}`]: target.dataset.value,
+  //   }));
+  // };
+
+  const regionDisplay = (value: string) => {
+    if (value === "user") {
+      //TODO: 유저의 정보값 넣어야
+      return user.userRegion;
+    } else {
+      return "전체";
+    }
+  };
+  const handleChangeDropdown = (e: React.SyntheticEvent) => {
+    const target = e.target as HTMLAnchorElement;
+    setFilter((prevFilter) => ({
+      ...prevFilter,
+      [`${target.dataset.title}`]: target.dataset.value,
+    }));
+
+    // if (target.dataset.title === "region") {
+    //   wishRegion = regionDisplay(target.dataset.value);
+    // }
+
+    // console.log(target.dataset.title, target.dataset.value);
+    // switch (target.dataset.title) {
+    //   case "category":
+    //     categoryName = target.dataset.value;
+    //     break;
+    //   case "region":
+    //     wishRegion = target.dataset.value;
+    //     break;
+    //   case "status":
+    //     status = target.dataset.value;
+    //     break;
+    // }
+    //console.log(categoryName, "dddd");
+    //queryClient.invalidateQueries("rentalProducts");
+  };
+
+  const handleChangeKeyword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const target = e.target as HTMLInputElement;
+    setFilter((prevFilter) => ({
+      ...prevFilter,
+      [`${target.name}`]: target.value,
+    }));
+  };
+
+  const handleSearch = () => {
+    console.log(filter);
+    setFilter({
+      ...filter,
+      ["keyword"]: "",
+    });
+  };
+  const [dropDown, setDropDown] = useState(false);
+  const categoryDisplay = (categoryEn: string) => {
+    switch (categoryEn) {
+      case "CLOTHING":
+        return "의류";
+      case "HOME":
+        return "생활가전";
+      case "FURNITURE":
+        return "가구/인테리어";
+      case "DIGITAL":
+        return "디지털기기";
+      case "GAMEANDRECORD":
+        return "게임/음반";
+      case "BOOK":
+        return "도서";
+      default:
+        return "전체";
+    }
+  };
+
+  const size = 6;
+
+  // const {
+  //   data,
+  //   isSuccess,
+  //   hasNextPage,
+  //   fetchNextPage,
+  //   isFetchingNextPage,
+  //   refetch,
+  //   isError,
+  //   isLoading,
+  // } = useInfiniteQuery(
+  //   ["rentalProducts", filter],
+
+  //   ({ pageParam = 1 }) =>
+  //     getProducts(
+  //       filter.category,
+  //       filter.region,
+  //       //wishRegion,
+  //       filter.keyword,
+  //       filter.status,
+  //       pageParam,
+  //       size
+  //     ),
+  //   {
+  //     getNextPageParam: (lastPage, allPages) => {
+  //       const nextPage = allPages.length + 1;
+  //       return nextPage;
+  //     },
+  //     onSuccess(data) {
+  //       console.log(filter);
+  //       //console.log("success", data);
+  //       //setPageData((prev) => ({ ...prev, ...data }));
+  //     },
+
+  //     refetchIntervalInBackground: true,
+  //   }
+  // );
+
+  console.log(data);
+  // useEffect(() => {
+  //   let fetching = false;
+  //   const handleScroll = async (e: any) => {
+  //     const { scrollHeight, scrollTop, clientHeight } =
+  //       e.target.scrollingElement;
+  //     if (!fetching && scrollHeight - scrollTop <= clientHeight * 1.2) {
+  //       fetching = true;
+  //       //console.log("fetching is true");
+  //       if (hasNextPage) await fetchNextPage();
+  //       //setPageData({ ...data });
+  //       fetching = false;
+  //     }
+  //   };
+  //   document.addEventListener("scroll", handleScroll);
+  //   return () => {
+  //     document.removeEventListener("scroll", handleScroll);
+  //   };
+  // }, [fetchNextPage, hasNextPage]);
+
+  const [test, setTest] = useState(true);
+  const [pageData, setPageData] = useState<InfiniteData<RentalProduct[]>>();
+
+  // 테스트 떄문에 임시로 주석
+
+  // useEffect(() => {
+  //   // setPageData((prev) => ({
+  //   //   ...data,
+  //   // }));
+  //   console.log("filterchanged");
+  //   console.log(filter);
+  //   console.log(isSuccess, data, isError);
+  //   //queryClient.invalidateQueries("rentalProducts");
+  //   //refetch();
+  //   //queryClient.resetQueries("rentalProducts");
+
+  //   //fetchNextPage({ pageParam: 1 });
+  //   //setTest((prev) => !prev);
+  // }, [filter]);
+  //console.log("test확인", data);
+  //console.log(isSuccess, data, isError);
+  return (
+    <>
+      <div className="flex flex-col">
+        {/* <div className="flex justify-between px-5 md:px-28 lg:px-40"></div> */}
+        <div className="flex items-center justify-between px-5 md:px-28 lg:px-40">
+          <ul className="menu menu-horizontal bg-base-100 z-50 text-sm">
+            <li tabIndex={0}>
+              <span className="whitespace-nowrap">
+                <Bars3Icon className="h-6 w-6" />
+                {categoryDisplay(filter.category)}
+              </span>
+              <ul className={`bg-base-100 rounded-box drop-shadow z-50`}>
+                <li>
+                  <a
+                    className={filter.category === "" ? "active" : ""}
+                    data-title="category"
+                    data-value=""
+                    onClick={handleChangeDropdown}
+                  >
+                    전체
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className={filter.category === "CLOTHING" ? "active" : ""}
+                    data-title="category"
+                    data-value="CLOTHING"
+                    onClick={handleChangeDropdown}
+                  >
+                    의류
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className={filter.category === "HOME" ? "active" : ""}
+                    data-title="category"
+                    data-value="HOME"
+                    onClick={handleChangeDropdown}
+                  >
+                    생활가전
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className={filter.category === "FURNITURE" ? "active" : ""}
+                    data-title="category"
+                    data-value="FURNITURE"
+                    onClick={handleChangeDropdown}
+                  >
+                    가구/인테리어
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className={filter.category === "DIGITAL" ? "active" : ""}
+                    data-title="category"
+                    data-value="DIGITAL"
+                    onClick={handleChangeDropdown}
+                  >
+                    디지털기기
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className={filter.category === "BOOK" ? "active" : ""}
+                    data-title="category"
+                    data-value="BOOK"
+                    onClick={handleChangeDropdown}
+                  >
+                    도서
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className={
+                      filter.category === "GAMEANDRECORD" ? "active" : ""
+                    }
+                    data-title="category"
+                    data-value="GAMEANDRECORD"
+                    onClick={handleChangeDropdown}
+                  >
+                    게임/음반
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li tabIndex={0}>
+              <span>
+                <MapIcon className="h-5 w-5 " />
+                <p>{regionDisplay(filter.region)}</p>
+              </span>
+
+              <ul className={`bg-base-100 rounded-box drop-shadow z-50`}>
+                <li className="min-h-fit">
+                  <a
+                    className={filter.region === "" ? " active" : ""}
+                    data-title="region"
+                    data-value=""
+                    onClick={handleChangeDropdown}
+                  >
+                    전체
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className={
+                      filter.region === user.userRegion ? " active" : ""
+                    }
+                    data-title="region"
+                    data-value={user.userRegion}
+                    onClick={handleChangeDropdown}
+                  >
+                    {user.userRegion}
+                  </a>
+                </li>
+              </ul>
+            </li>
+
+            <li tabIndex={0}>
+              <span className="whitespace-nowrap">
+                <FlagIcon className="h-5 w-5 " />
+                {productStatus(filter.status)}
+              </span>
+              <ul className={` bg-base-100 rounded-box drop-shadow z-50`}>
+                <li className="min-h-fit">
+                  <a
+                    className={filter.status === "" ? "active" : ""}
+                    data-title="status"
+                    data-value=""
+                    onClick={handleChangeDropdown}
+                  >
+                    전체
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className={
+                      filter.status === "AVAILABLE"
+                        ? "text-success active"
+                        : "text-success"
+                    }
+                    data-title="status"
+                    data-value="AVAILABLE"
+                    onClick={handleChangeDropdown}
+                  >
+                    대여가능
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className={
+                      filter.status === "WAITING"
+                        ? "text-warning active"
+                        : "text-warning"
+                    }
+                    data-title="status"
+                    data-value="WAITING"
+                    onClick={handleChangeDropdown}
+                  >
+                    반납대기
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className={
+                      filter.status === "RENTED"
+                        ? "text-error active"
+                        : "text-error"
+                    }
+                    data-title="status"
+                    data-value="RENTED"
+                    onClick={handleChangeDropdown}
+                  >
+                    대여중
+                  </a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+          <div className="form-control">
+            <div className="input-group input-group-sm">
+              <input
+                type="text"
+                placeholder="상품 키워드 검색"
+                className="input input-sm input-bordered input-primary"
+                name="keyword"
+                value={filter.keyword}
+                onChange={handleChangeKeyword}
+              />
+              <button
+                className="btn btn-sm btn-square btn-outline btn-primary"
+                onClick={handleSearch}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="px-5 md:px-28 lg:px-40">
+          {/* {isLoading && (
+            <div className="h-full flex items-center justify-center">
+              <p>Loading...</p>
+              <progress className="progress w-56"></progress>
+            </div>
+          )}
+          {isError && (
+            <div className="h-full flex items-center justify-center">
+              <p>Error...</p>
+            </div>
+          )} */}
+
+          <ProductList data={data} />
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ src/pages/RentalDetailPage copy.tsx
@@ -0,0 +1,311 @@
+import React, { useEffect, useState } from "react";
+import { ChatBubbleBottomCenterTextIcon } from "@heroicons/react/24/outline";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import {
+  deleteProduct,
+  getProduct,
+} from "../api/rentalProduct/rentalProductAPI";
+import { useMutation, useQuery } from "react-query";
+import Badge from "../components/Badge";
+import sample404 from "../assets/404sample.png";
+import profileSample from "../assets/profile_sample.png";
+import { maxRentalPeriod, categoryName } from "../utils/converter";
+import { RentalProductDetail } from "../types/product";
+import moment from "moment";
+import ReactDOM from "react-dom";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+import { useRecoilState, useRecoilValue } from "recoil";
+import axios from "axios";
+import { userInfo } from "../atoms/userInfo";
+import { token } from "../atoms/token";
+
+// const deleteMutation = (productId: string) => {
+//   //useMutation("deleteProduct", () => deleteProduct(productId), {
+//   useMutation("deleteProduct", async () => await deleteProduct(productId), {
+//     onSuccess: (res) => {
+//       console.log("response test", res);
+//       // Invalidate and refetch
+//       //queryClient.invalidateQueries("todos");
+//     },
+//   });
+// };
+
+// 테스트
+
+const data = {
+  categoryName: "HOME",
+  content: "새거새거",
+  createdAt: "2023-03-31T14:40:27.79989",
+  id: 1,
+  mainImageUrl: "6f67cf96-56b2-4f58-8",
+  maxRentalPeriod: "ONEMONTH",
+  modifiedAt: "2023-03-31T14:40:27.799921",
+  nickname: "레모나",
+  returnDate: new Date("2023-4-6"),
+  sellerId: "aa123@gmail.com",
+  status: "AVAILABLE",
+  title: "냉장고",
+  unitPrice: 12300,
+  wishRegion: "서울 도봉구 노해로 133",
+};
+
+// 테스트
+
+export default function RentalDetailPage() {
+  const productId = useParams().id;
+  //console.log(id);
+  // const { isLoading, isError, data, error }: any = useQuery(
+  //   "productDetail",
+  //   () => getProduct(productId)
+  // );
+
+  //TODO: 로그인 없이 임시 테스트를 위한 유저 info - 유저정보로 바꿔야
+  // const [user, setUser] = useRecoilState(userInfo);
+
+  // const productDetail: RentalProductDetail | undefined = data as
+  //   | RentalProductDetail
+  //   | undefined;
+  //console.log(productDetail);
+  const onErrorImg = (e: any) => {
+    e.target.src = sample404;
+  };
+  const onErrorProfile = (e: any) => {
+    e.target.src = profileSample;
+  };
+
+  // const [deleteMutation] = useMutation(deletePost, {
+  //   onSuccess: () => {
+  //     // 콜백 함수에서 적절한 값을 반환하도록 수정
+  //     return true;
+  //   },
+  // });
+
+  const handleDelete = (id: string) => {
+    deleteMutation.mutate(id);
+  };
+  useEffect(() => {
+    // setUser({
+    //   userEmail: "pepe@gmail.com",
+    //   userNickName: "개구리페페",
+    //   userRegion: "서울시 도봉구",
+    //   userProfileImage:
+    //     "https://blog.kakaocdn.net/dn/wR5bN/btqSxCsIZD8/0g1pTeaqRwXKvBcxPtqQE0/img.jpg",
+    // });
+  }, []);
+
+  // react carousel map 관련 문제 해결 테스트
+  const testUrls = [
+    "https://blog.kakaocdn.net/dn/wR5bN/btqSxCsIZD8/0g1pTeaqRwXKvBcxPtqQE0/img.jpg",
+    "https://t1.daumcdn.net/cfile/tistory/992755335A157ED62B",
+    "https://img1.daumcdn.net/thumb/C176x176/?fname=https://blog.kakaocdn.net/dn/cv7UPT/btrSm0BDg2h/bigd2G5zkOERd0S1KEq3Ek/img.jpg",
+  ];
+
+  // 테스트
+  const { userNickName } = useRecoilValue(userInfo);
+  console.log(userNickName);
+  const navigate = useNavigate();
+  const [rentalRequest, setRentalRequest] = useState(false);
+
+  const deleteMutation = useMutation(
+    "deleteProduct",
+    async (productId: string) => {
+      const res = await deleteProduct(productId);
+      return res;
+    },
+    {
+      onSuccess: (res) => {
+        console.log("response test", res);
+      },
+    }
+  );
+
+  const tokenKey = useRecoilValue(token);
+  const moveChatRoom = useMutation(
+    async () => {
+      return await axios({
+        method: "post",
+        url: "http://43.200.141.247:8080/chat/room",
+        headers: { Authorization: tokenKey },
+        data: {
+          receiverNickname: data.nickname,
+          senderNickname: userNickName,
+          product: {
+            ...data,
+            sellerId: {
+              email: data.sellerId,
+              nickName: data.nickname,
+              region: data.wishRegion,
+              title: data.title,
+              imageUrl: testUrls[0],
+            },
+          },
+          // rentalRequest: rentalRequest,
+        },
+      }).then((res) => res.data);
+    },
+    {
+      onSuccess: (data) => {
+        // roomId 서버에서 보내줌
+        navigate(`/chat/room/${data}?senderId=${userNickName}`);
+        console.log("문의하기");
+      },
+      onError: () => {
+        console.log("통신 에러 발생!");
+      },
+    }
+  );
+
+  const loginUserInfo = useRecoilValue(userInfo);
+
+  // 채팅방 입장
+  const rental = (event: React.MouseEvent<HTMLButtonElement>) => {
+    if (event.currentTarget.name === "rentalRequest") {
+      setRentalRequest((prev) => !prev);
+      moveChatRoom.mutate();
+    } else {
+      moveChatRoom.mutate();
+    }
+  };
+
+  return (
+    <>
+      {/* {isLoading && <div>Loading...</div>} */}
+      {data && (
+        <div className="container flex justify-center px-5 md:px-40 lg:px-56">
+          <div className="flex flex-col">
+            <div className="w-full min-w-[33rem] h-96 overflow-hidden rounded relative">
+              <div className="carousel w-full">
+                {/* {productDetail.imageUrls.map((url, index) => (   */}
+                {testUrls.map((url, index) => (
+                  <div
+                    id={`slide${index}`}
+                    key={`slide${index}`}
+                    className="carousel-item relative w-full h-96"
+                  >
+                    <img
+                      //로컬
+                      src={url}
+                      //src={`https://dj8fgxzkrerlh.cloudfront.net/${url}`}
+                      className="object-cover w-full h-full"
+                    />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                      <a
+                        // href={`#slide${productDetail.imageUrls.length - 1}`}
+                        href={`#slide${
+                          index === 0 ? testUrls.length - 1 : index - 1
+                        }`}
+                        onClick={() => console.log(testUrls.length - 1)}
+                        className="btn btn-circle"
+                      >
+                        ❮
+                      </a>
+                      <a
+                        href={`#slide${
+                          index === testUrls.length - 1 ? index - 1 : index + 1
+                        }`}
+                        className="btn btn-circle"
+                        onClick={() => console.log(index + 1)}
+                      >
+                        ❯
+                      </a>
+                    </div>
+                  </div>
+                ))}
+              </div>
+
+              <Badge value={data.status} isFull={true} />
+            </div>
+            <div className="flex flex-col">
+              <section className="flex justify-between items-center w-full pb-3 pt-4">
+                <div className="flex gap-x-3 items-center ">
+                  <div className="avatar">
+                    <div className="w-10 h-10 rounded-full">
+                      <img
+                        src={loginUserInfo.userProfileImage}
+                        onError={onErrorProfile}
+                      />
+                    </div>
+                  </div>
+                  <div className="text-sm">
+                    <p>{data.nickname}</p>
+                    <p>{data.wishRegion}</p>
+                  </div>
+                </div>
+                <div>
+                  {loginUserInfo.userEmail === data.sellerId ? (
+                    <button
+                      className="btn btn-sm btn-outline btn-error py-0"
+                      // onClick={() => handleDelete(data.id)}
+                    >
+                      상품삭제
+                    </button>
+                  ) : (
+                    <button
+                      // to="/chat/room/roomid"
+                      name="enquiry"
+                      className="btn btn-sm btn-outline btn-primary"
+                      onClick={rental}
+                    >
+                      <ChatBubbleBottomCenterTextIcon className="h-3 w-3 mr-0.5" />
+                      상품문의
+                    </button>
+                  )}
+                </div>
+              </section>
+              <div className="divider my-0"></div>
+              <section className="mt-3">
+                <h1 className="text-xl font-semibold mb-1">{data.title}</h1>
+                <p className="text-sm text-neutral mb-1">
+                  {categoryName(data.categoryName)} ∙
+                  <span>{moment(data.modifiedAt).format("YYYY-MM-DD")}</span>
+                </p>
+                <div className="flex items-center">
+                  <span className="text-lg font-semibold ">
+                    {data.unitPrice} 원 / 일
+                  </span>
+                  <span className="text-sm font-regular ml-3">
+                    * 최대 {maxRentalPeriod(data.maxRentalPeriod)}일 대여가능
+                  </span>
+                  {data.returnDate ? (
+                    <span className="text-xs text-error ml-3">
+                      * 반납까지
+                      {moment(data.returnDate).from(Date.now(), true)}
+                      남음
+                    </span>
+                  ) : (
+                    ""
+                  )}
+                </div>
+                <p className="mt-5 font-normal">{data.content}</p>
+              </section>
+              <div className="divider"></div>
+              <section>
+                <h3 className="text-lg font-semibold">거래 희망지역</h3>
+                <p>{data.wishRegion}</p>
+                <p>여건이 되면 이곳에 지도</p>
+              </section>
+              <section>
+                {loginUserInfo.userEmail === data.sellerId ? (
+                  <button
+                    disabled
+                    className="btn btn-primary sm:btn-sm lg:btn-md w-full mt-3"
+                  >
+                    렌탈하기
+                  </button>
+                ) : (
+                  <Link
+                    to={`/product/pay/${data.id}`}
+                    className="btn btn-primary sm:btn-sm lg:btn-md w-full mt-3"
+                  >
+                    렌탈하기
+                  </Link>
+                )}
+              </section>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
