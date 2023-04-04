aae7c266603105db28e955a75a83d482abcec727
Jeong-Yeon-Lee <0906ljy@gmail.com>
2023-04-04 20:24:10 +0900

    :sparkles: feat: 유저 권한에 따라 라우팅

--- src/App.tsx
+++ src/App.tsx
@@ -12,37 +12,88 @@ import RentalCreatePage from "./pages/RentalCreatePage";
 import RentalDetailPage from "./pages/RentalDetailPage";
 import RentalReturnPage from "./pages/RentalReturnPage";
 import RentalPayPage from "./pages/RentalPayPage";
-// import Test from "./Test";
-// import ScrollTest from "./pages/ScrollTest";
 import axios from "axios";
 import Chat from "./pages/Chat";
 import ChatList from "./pages/ChatList";
 import NotFound from "./pages/NotFound";
 import RentalDetailTestPage from "./pages/RentalDetailTestPage";
-
+import { useRecoilValue } from "recoil";
+import { token } from "./atoms/token";
 // axios에 withCredentials를 true로 설정해줘야 refreshToken cookie를 주고받을 수 있다.
 axios.defaults.withCredentials = false;
 
 function App() {
+  const tokenValue: string = useRecoilValue(token);
+
   return (
     <Layout>
       <Routes>
-        <Route path="/main/*" element={<MainPage />} />
-        <Route path="/product/create/*" element={<RentalCreatePage />} />
-        <Route path="/product/detail/:id" element={<RentalDetailPage />} />
-        <Route path="/product/return/:id" element={<RentalReturnPage />} />
-        <Route path="/product/pay/:id" element={<RentalPayPage />} />
-        <Route path="/chat/list" element={<ChatListPage />} />
         <Route path="/login/*" element={<LoginPage />} />
         <Route path="/signup/*" element={<SignUpPage />} />
-        <Route path="/my/*" element={<MyPage />} />
-        <Route path="/my/products/*" element={<MyRentalHistoryPage />} />
-        {/* <Route path="/test/*" element={<ScrollTest />} /> */}
-        {/* <Route path="/mock/test/*" element={<Test />} /> */}
-        <Route path="/chat/room/:roomId" element={<Chat />} />
-        {/* <Route path="/chat/list" element={<ChatList />} /> */}
         <Route path="/" element={<Navigate replace to="/main" />} />
         <Route path="*" element={<NotFound />} />
+        <Route
+          path="/main/*"
+          element={tokenValue ? <MainPage /> : <Navigate replace to="/login" />}
+        />
+        <Route
+          path="/product/create/*"
+          element={
+            tokenValue ? <RentalCreatePage /> : <Navigate replace to="/login" />
+          }
+        />
+        <Route
+          path="/chat/list"
+          element={
+            tokenValue ? <ChatListPage /> : <Navigate replace to="/login" />
+          }
+        />
+        <Route
+          path="/product/detail/:id"
+          element={
+            tokenValue ? <RentalDetailPage /> : <Navigate replace to="/login" />
+          }
+        />
+        <Route
+          path="/product/return/:id"
+          element={
+            tokenValue ? <RentalReturnPage /> : <Navigate replace to="/login" />
+          }
+        />
+        <Route
+          path="/product/pay/:id"
+          element={
+            tokenValue ? <RentalPayPage /> : <Navigate replace to="/login" />
+          }
+        />
+        <Route
+          path="/my/*"
+          element={tokenValue ? <MyPage /> : <Navigate replace to="/login" />}
+        />
+        <Route
+          path="/my/products/*"
+          element={
+            tokenValue ? (
+              <MyRentalHistoryPage />
+            ) : (
+              <Navigate replace to="/login" />
+            )
+          }
+        />
+        <Route
+          path="/chat/room/:roomId"
+          element={tokenValue ? <Chat /> : <Navigate replace to="/login" />}
+        />
+        <Route
+          path="/my/products/*"
+          element={
+            tokenValue ? (
+              <MyRentalHistoryPage />
+            ) : (
+              <Navigate replace to="/login" />
+            )
+          }
+        />
       </Routes>
     </Layout>
   );
