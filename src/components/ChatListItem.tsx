df8a728a58a42d296bb56231d8c935bf5666ca82
jjunghoo <wjdgn2468@naver.com>
2023-04-06 01:00:54 +0900

    :sparkles: feat: 구매자와 판매자간의 채팅방 나가기
    채팅방안에서 채팅방 나가기, 채팅 목록에서 채팅방 나가기

--- src/components/ChatListItem.tsx
+++ src/components/ChatListItem.tsx
@@ -2,12 +2,35 @@ import { Link } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 import { userInfo } from "../atoms/userInfo";
 import moment from "moment";
+import axios from "axios";
+import { useQueryClient } from "react-query";
+import { useState } from "react";
+
 export const ChatListItem = ({ list }: any) => {
   const loginUserInfo = useRecoilValue(userInfo);
+  const queryClient = useQueryClient();
+  //TODO: 채팅방 나가기
+  // 따로 분리 시켜야 함.
+  const leaveChatRoom = async (roomId: string) => {
+    await axios.delete(`https://rentalmarket.monster/chat/room/${roomId}`, {
+      // headers: {
+      //   Authorization: accessToken,
+      // },
+    });
+
+    //TODO: 체크해야 할 부분 23.04.05 (수) - 채팅 리스트 페이지 바로 적용 안됨
+    await queryClient.invalidateQueries("chatList");
+  };
+
+  const [hover, setHover] = useState(false);
 
   return (
     <>
-      <li className="card w-fit bg-base-100  rounded-none hover:bg-neutral-100 ease-out duration-100 cursor-pointer">
+      <li
+        className="card w-fit bg-base-100  rounded-none hover:bg-neutral-100 ease-out duration-100 cursor-pointer relative"
+        onMouseEnter={() => setHover((prev) => !prev)}
+        onMouseLeave={() => setHover((prev) => !prev)}
+      >
         <Link
           to={`/chat/room/${list.roomId}?nickname=${loginUserInfo.userNickName}`}
         >
@@ -40,6 +63,14 @@ export const ChatListItem = ({ list }: any) => {
             </div>
           </div>
         </Link>
+        {hover && (
+          <button
+            className="btn btn-ghost text-error btn-xs md:btn-sm text-lg lg:text-xl absolute top-0 right-0"
+            onClick={() => (list ? leaveChatRoom(list.roomId) : "")}
+          >
+            ×
+          </button>
+        )}
       </li>
     </>
   );
